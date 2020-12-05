65d21f0f397f20875e020e0180ed805650c91b0c
johnnyy <johnnymarcos1998@gmail.com>
2020-12-05 16:47:39 -0300

    atividade02

---
+++ atividades/atividade02/maiorQue10mb.sh
@@ -0,0 +1,8 @@
+mkdir maiorque10
+find pasta_arquivos/ -size +10000k -exec cp {} maiorque10/ \;
+tar -czf maiorque10.tar.gz maiorque10/
+rm -r maiorque10
+
+
+#Usei +10000k pq com o uso do +10M os arquivos maiores que 10mb e menores que 11mb não estavam sendo selecionados
+#Creio que pelo uso da forma interia do +10M, revendo tudo menor que 11M
