c442e95ace2d7e5def833e2df82a603baf6a1950
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-08 21:35:14 -0300

    Update parte_01.sh
    Correção da parte 01.

--- exercicios/exercicio02/parte_01.sh
+++ exercicios/exercicio02/parte_01.sh
@@ -1,4 +1,5 @@
 grep -E "[0-9]+ A" emailsordenados.txt
+# Você deveria ter utilizado o marcador \b para indicar a busca no início das palavras. 
 grep -E " A" emailsordenados.txt
 grep -E ".br$" emailsordenados.txt 
 grep -E " [^A-Z].*[1-9]+.*@" emailsordenados.txt
