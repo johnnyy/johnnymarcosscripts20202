d9f64b39aea31e41d2bf5de7bded06d20e163556
johnnyy <johnnymarcos1998@gmail.com>
2020-12-15 20:23:31 -0300

    exercicio 03

--- exercicios/exercicio03/saudacao.sh
+++ exercicios/exercicio03/saudacao.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e  $"Olá $(who | cut -d' ' -f1), \nHoje é dia $(date +%d), do mês $(date +%m) do ano de $(date +%G)." | tee -a saudacao.log
+echo -e  $"Olá $(whoami), \nHoje é dia $(date +%d), do mês $(date +%m) do ano de $(date +%G)." | tee -a saudacao.log
