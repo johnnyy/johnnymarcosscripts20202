25468fca96b6ef30539f6e61c05e5341f3e36170
johnnyy <johnnymarcos1998@gmail.com>
2021-01-16 21:26:06 -0300

    Atividade 08

---
+++ atividades/atividade08/organizarPorData.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in $( ls $1 )
+do
+
+	ano=$(stat $1/${file}  -c %y | cut -d ' ' -f1 | cut -d '-' -f1)
+	mes=$(stat $1/${file}  -c %y | cut -d ' ' -f1 | cut -d '-' -f2)
+	dia=$(stat $1/${file}  -c %y | cut -d ' ' -f1 | cut -d '-' -f3)
+	
+	[ -d $2/${ano}/${mes}/${dia} ] ||  mkdir -p $2/${ano}/${mes}/${dia}
+	cp $1/${file} $2/${ano}/${mes}/${dia}/
+
+done
