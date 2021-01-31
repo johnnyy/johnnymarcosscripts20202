d7016a7d4d940e0241e91a751a2ed7bc9f6fb1f4
johnnyy <johnnymarcos1998@gmail.com>
2021-01-30 22:55:26 -0300

    Atividade 10

---
+++ atividades/atividade10/contaPalavras.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+read -p "Informe o arquivo: " ARQUIVO
+
+
+if [ -f $ARQUIVO ]
+then
+	cat $ARQUIVO |sed -s 's/[[:punct:]]//g' | tr -s ' ' '\n' | sed -s '/^$/d' | sort |  uniq -c | sed -s 's/[ ]\{2,\}//g' > terms_counts_value.aux
+
+	while read line
+	do
+		number=$( echo "$line" | cut -d ' ' -f1)
+		word=$( echo "$line" | cut -d ' ' -f2)
+		echo ${word}: ${number}
+	done < terms_counts_value.aux
+	
+	rm terms_counts_value.aux
+else
+	printf "Arquivo não encontrado.\n "
+
+fi
